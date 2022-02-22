7f3d8bfe8e6d56cd5a52890f6bc3d18c13a48f5b
marcoswlrich <marcosapw.dev@gmail.com>
2022-02-21 23:55:23 -0300

    findUserById

--- src/index.js
+++ src/index.js
@@ -61,7 +61,17 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userId = users.find((user) => user.id === id);
+
+  if (!userId) {
+    return response.status(404).json({ error: "User does not exist!" });
+  }
+
+  request.user = userId;
+
+  return next();
 }
 
 app.post("/users", (request, response) => {
